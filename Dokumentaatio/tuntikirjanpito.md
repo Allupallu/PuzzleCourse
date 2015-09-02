5f3072870a12e119480eab5b57b42955b59a68e4
Aleksi Karpansalo <aleksi.karpansalo@helsinki.fi>
2015-09-02 13:05:11 +0300

    Tynkä lisätty.

--- Dokumentaatio/tuntikirjanpito.md
+++ Dokumentaatio/tuntikirjanpito.md
@@ -1,5 +1,6 @@
 # Tuntikirjanpito
 
-## Ke 2.9. - 2 tuntia 20 min
+## Ke 2.9. - 2 tuntia 30 min
 - 10:00 - 11:50: Tutkin tehtävänantoa ja laitoin rakenteet ja välineet kuntoon.
 - 12:00 - 12:30: Palasin ruokailemasta kirjoittamaan dokumentaatiota.
+- 12:50 - 13:00: Tein ohjelman tyngän.
---
+++ PuzzleCourse/src/main/java/UI/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+/**
+ * Ohjelman tynkä.
+ * Jokaisen palautuksen yhteydessä ohjelma tulee voida ajaa ilman 
+ * virheilmoituksia, vaikka se ei tekisi vielä mitään. Täten vainoharhaiset
+ * opiskelijat tekevät ohjelman tyngän ensimmäiseen palautukseen. :D
+ * @author aleksi
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("You'll never get me alive, coppers!");
+    }
+    
+}
